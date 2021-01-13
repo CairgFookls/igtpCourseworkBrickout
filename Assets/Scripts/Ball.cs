49809f3e11c2f4258b27f4881ba71df3a27c1dca
CairgFookls <73195892+CairgFookls@users.noreply.github.com>
2021-01-13 20:59:04 +0000

    Add files via upload

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    //Written By Craig Foulkes
+    //Last Updated 11/01/2021
+
+    // Declares variables and Initialises them.
+    float speed = 0.03f;
+    float directionY = 2.0f;
+    float directionX = 1.3f;
+
+    int totalPoints = 0;
+    int points = 0;
+    int lives = 3;
+    
+
+    // Declares text UI variables.
+    public Text pointTracker;
+    public Text liveTracker;
+
+    // Sets the text that displays points text portion of the UI to display "points: 0" on startup
+    void Start()
+    {
+        pointTracker.text = "Points:" + points.ToString();
+        liveTracker.text = "Lives:" + lives.ToString();
+    }
+
+    // Updates the position of the ball
+    void FixedUpdate()
+    {
+
+        Vector3 position = transform.localPosition;
+        position.y -= speed * directionY;
+        position.x += speed * directionX;
+        transform.localPosition = position;
+      
+    }
+
+    // Ball Collision detection
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Sets the properties of collisions of the 4 walls and the bat
+        switch (other.gameObject.name)
+        {
+            // Causes the ball's Y direction to reverse after colliding with the top wall or the bat
+            case "Bat":
+            case "Wall (top)":
+                directionY = -directionY;
+                break;
+            
+            // Causes the ball's X direction to reverse after colliding with the side walls
+            case "Wall (right)":
+            case "Wall (left)":
+                directionX = -directionX;
+                break;
+            
+            // resets the balls position and direction as well as reduces the number of lives after colliding with the kill box. When the player runs out of lives, the ball is deleted and the life tracking text displays "Game Over!"
+            case "Kill box":
+                transform.position = new Vector3(0, -2, 0);
+                directionY = 2.0f;
+                directionX = 1.3f;
+
+                lives -= 1;
+                liveTracker.text = "Lives:" + lives.ToString();
+
+                if (lives == 0)
+                {
+                    liveTracker.text = "Game Over!";
+                    Destroy(gameObject);
+                }
+                break;
+
+            
+        }
+
+        // If the ball collides with a brick, the brick is destroyed, the ball's Y direction is reversed and points are added to the player's total, displaying on the Points text portion of the UI.
+        // Hitting a brick also increases the speed variable of the ball, making the ball go faster the further you progress.
+        // Destroying all of the bricks deletes the ball and sets the life tracking text to display "You Won!"
+        if(other.gameObject.tag == "Brick")
+        {
+            Destroy(other.gameObject);
+            directionY = -directionY;
+
+            speed += 0.0005f; 
+
+            AddPoints(10);
+
+            if (totalPoints == 440)
+            {
+                liveTracker.text = "You Won!";
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    // adds points to the player's point total when called
+    void AddPoints(int points)
+    {
+        totalPoints += points;
+        pointTracker.text = "Points:" + totalPoints.ToString();
+    }
+
+}
---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class Bat : MonoBehaviour
+{
+    //Written by Craig Foulkes
+    //Last updated 11/01/21
+
+    // initialises speed and direction variables
+    public float speed = 0.02f;
+    float direction = 0.0f;
+
+    // Initialises left and right movement key codes to the A and D key respectively
+    public KeyCode moveLeftKey = KeyCode.A;
+    public KeyCode moveRightKey = KeyCode.D;
+
+    // Initialises boolean variables to true
+    bool canMoveLeft = true;
+    bool canMoveRight = true;
+
+
+    // Updates the position of the bat.
+    void FixedUpdate()
+    {
+        Vector3 position = transform.localPosition;
+        position.x += speed * direction;
+        transform.localPosition = position;
+    }
+
+
+    //updates the direction variable depending on player input, causing the bat to move left or right (assuming the bat hasn't collided with a side wall) or stay still.
+    void Update()
+    {
+
+        bool isLeftPressed = Input.GetKey(moveLeftKey);
+        bool isRightPressed = Input.GetKey(moveRightKey);
+
+        if (isLeftPressed && canMoveLeft)
+        {
+            direction = -1.0f;
+        }
+        else if (isRightPressed && canMoveRight)
+        {
+            direction = 1.0f;
+        }
+        else
+        {
+            direction = 0.0f;
+        }
+    }
+
+    // When the bat collides with a side wall, this code renders the bat unable to move any further in the direction of the wall it has collided with.
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Wall (left)":
+                canMoveLeft = false;
+                break;
+
+            case "Wall (right)":
+                canMoveRight = false;
+                break;
+        }
+    }
+
+    // When the bat no longer makes contact with a side wall, it is able to travel in the direction of the side wall it collided with once again.
+    void OnCollisionExit2D(Collision2D other)
+    {
+        switch (other.gameObject.name)
+        {
+            case "Wall (left)":
+                canMoveLeft = true;
+                break;
+
+            case "Wall (right)":
+                canMoveRight = true;
+                break;
+        }
+    }
+}
